03b33c2d3b5f0aa1ddda9c2623960a1e8fa6fc13
tkurudi <t.kurudi209@gmail.com>
2019-03-05 15:21:23 +1300

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Tarun:Cricket99@mern-ziorb.mongodb.net/test?retryWrites=true'
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile')
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongodb
+mongoose
+.connect(db)
+.then(() => console.log('mongoDB connected'))
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('hello'))
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
