cc714e21e96f09de62751e1386c6f697c0e09891
tkurudi <t.kurudi209@gmail.com>
2019-03-06 18:20:00 +1300

    error

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+// load profile model
+const Profile = require('../../models/Profile');
+// load user model
+const User = require('../../models/User');
 
 //@route         GET api/profile/test
 // @desc         tests post route
 //@access        Public
 
 router.get('/test' , (req, res) => res.json({msg: "profile works"}));
+
+//@route         GET api/profile/profile
+// @desc         get current users profile
+//@access        Private
+
+router.get('/', passport.authenticate('jwt', {session: false}), (res, req) => {
+            const errors = {}
+            console.log(user)
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'there is no profile'
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err))
+});
 
 module.exports = router;
