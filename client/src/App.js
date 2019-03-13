e422ce6b2f9cdc219ecf3d7c95a412f8e9718176
tkurudi <t.kurudi209@gmail.com>
2019-03-13 16:40:51 +1300

    added router and auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Navbar />
+      <Route exact path="/" component={Landing}/>
+        <Footer/>
       </div>
+      </Router>
     );
   }
 }
