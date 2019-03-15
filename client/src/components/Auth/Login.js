4f4e6cdc4d771859289a48a3decf2166706066a2
tkurudi <t.kurudi209@gmail.com>
2019-03-15 13:10:13 +1300

    yesterdays work

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 
 export default class Login extends Component {
     state = {
-        name: '',
-        password: ''
+        email: '',
+        password: '',
+        error: {}
     }
 
     
--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Register extends Component {   
         state = {
@@ -23,7 +24,9 @@ export default class Register extends Component {
          password: this.state.password,
          password2: this.state.password2
      }
-     console.log(newUser)
+     axios.post('/api/users/register', newUser)
+         .then(res => console.log(res.data))
+         .catch(err => console.log(err))
  }
 
 
