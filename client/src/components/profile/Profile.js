acf4a5b338d570413ca531d2b1db58c38b881295
tkurudi <t.kurudi209@gmail.com>
2019-04-03 16:24:18 +1300

    added reducer and actions and types

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,7 +16,11 @@ import {getProfileByHandle} from '../../actions/profileActions';
             this.props.getProfileByHandle(this.props.match.params.handle)
         }
     }
-
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.profile.profile === null & this.props.profile.loading) {
+            this.props.history.push('/not-found')
+        }
+    }
 
   render() {
       const { profile, loading } = this.props.profile
